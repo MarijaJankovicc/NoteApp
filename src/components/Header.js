09adfc259cfb2024bd6c97cf2933c24578822a5f
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2022-10-17 14:24:19 +0200

    refactor: Refactor sort menu item

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,33 @@ import React from "react";
 import { MenuItem, Select} from "@mui/material";
 
 export function Header({ handleToggleDarkMode, handleSort}) {
+
+    const menuItem = [  
+        {
+            id: 1,
+            title: "Select",
+            value: "none",           
+        },
+        {
+            id: 2,
+            title: "ASCENDING",
+            value: "ascending"
+        },
+        {
+            id: 3,
+            title: "DESCENDING",
+            value: "descending"
+        }
+    ];
+
     return (
         <div className="header">
             <h1><span className="first-word">React </span><span>Notes</span></h1>
             <span className="rightHeader">     
-                <Select className="sortBtn" defaultValue={'sort'} 
-                    onChange={handleSort}> 
-                    <MenuItem value={'sort'} disabled>Sort</MenuItem>
-                    <MenuItem value={'ascending'}>ASCENDING</MenuItem>
-                    <MenuItem value={'descending'}>DESCENDING</MenuItem>            
+                <Select className="sortBtn" defaultValue={'none'} onChange={handleSort}> 
+                    {menuItem.map((item) => (
+                        <MenuItem disabled={item.id===1} key={item.id} value={item.value}>{item.title}</MenuItem>
+                    ))}
                 </Select>
                 <button className="btn" onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)}>Toggle Mode</button>
             </span>
