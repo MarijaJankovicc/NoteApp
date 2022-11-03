8f34d9a1b68add25bec42443c76da26c209f670d
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2022-11-03 13:25:27 +0100

    fix: Keep user when refreshed page

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,9 +1,10 @@
-import { React } from 'react';
-import { auth } from './firebase';
+import { React, useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { UserContext } from './UserProvider';
 
 const PrivateRoute = () => {
-  return auth.currentUser ? <Outlet/> : <Navigate to='/'/>;
+  const logged = useContext(UserContext);
+  return logged ? <Outlet/> : <Navigate to='/'/>;
 };
 
 export default PrivateRoute;
